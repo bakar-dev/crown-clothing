a0d085a98047019ee34309ac91f2f540f17bed17
Bakr Hamid <baker.pucit@gmail.com>
2019-11-20 18:46:22 +0500

    Sign in form added + Custom input and button added for form

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+import "./sign-in.styles.scss";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+export default class signIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = event => {
+    console.log("E E", event.target.value);
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+
+          <CustomButton type="submit" value="Submit">
+            Log In
+          </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+export default function signInAndSignUp() {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+}
