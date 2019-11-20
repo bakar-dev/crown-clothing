bab9b34b9afaa58269a6a55913d3f9e1f36969de
Bakr Hamid <baker.pucit@gmail.com>
2019-11-20 16:14:54 +0500

    adding header and links to shop and contact

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./header.styles.scss";
+
+export default function header() {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+
+      <div className="options">
+        <Link to="/shop" className="option">
+          SHOP
+        </Link>
+        <Link to="/contact" className="option">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+}
