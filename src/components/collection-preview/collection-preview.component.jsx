a2615672723b8b92f1585cf6d598a61045deb5b6
Bakr Hamid <baker.pucit@gmail.com>
2019-11-20 15:25:47 +0500

    Collection-item component added for shop page and for collection-preview-component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./collection-preview.styles.scss";
 
+import CollectionItem from "../collection-item/collection-item.component";
+
 export default function collectionPreview({ title, items }) {
   return (
-    <div>
-      <h1>{title.toUpperCase()}</h1>
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map(item => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
