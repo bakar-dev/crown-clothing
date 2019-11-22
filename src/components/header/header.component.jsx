f94e6e6492495bc6a91f191a5cf76167affaec9b
Bakr Hamid <baker.pucit@gmail.com>
2019-11-22 18:12:54 +0500

    added checkout page + checkout-item component with totalitemCost selector added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
 
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
@@ -42,9 +45,9 @@ function header({ currentUser, hidden }) {
   );
 }
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(header);
